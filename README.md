9c31ca58fc75b0d1c0d257b0aee23b0afb74793e
MarianaSDiaz <maruuus.95@gmail.com>
2021-11-23 16:50:19 -0300

    CocaCola-Project: entire project

---
+++ README.md
@@ -0,0 +1,6 @@
+#Coca Cola Project
+
+This is the final project of the Training HappyDev course. It's a Coca Cola's landing page.
+I used XAMPP to access the Apache Server to send an email from the "contáctenos" form.
+
+I want to thank Jacobo Amaru for helping me create this interesting project.
---
+++ js/funciones.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(listo);
+
+function listo() {
+        jQuery(".hamb").click(function(e){
+                e.preventDefault();
+                jQuery("header .container nav").toggleClass("open");
+                jQuery(".hamb i").toggleClass("fa-times");
+        });
+        jQuery("header .container nav a").click(function(){
+                jQuery(".hamb i").removeClass("fa-times");
+                jQuery("header .container nav").removeClass("open");
+                
+                let dev = jQuery(this).attr("href");
+                jQuery("html,body").animate({
+                        "scrollTop": jQuery(dev).offset().top - 76
+                })
+        })
+        jQuery("")
+}
+
